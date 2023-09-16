8f7de478ca7bdbf4caabbf71c514663030e0e0cf
Chris McKenzie <christophermckenzie.84@gmail.com>
2023-09-16 15:39:30 -0400

    select UI elements

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 import { pipeline } from 'https://cdn.jsdelivr.net/npm/@xenova/transformers@2.3.0';
 
+const longTextInput = document.getElementById('long-text-input');
+const generateButton = document.getElementById('generate-button');
+const output = document.getElementById('output-div');
+const spinner = document.getElementById('spinner');
+
 const summarization = await pipeline(
     'summarization', // task
     'Xenova/t5-small' // model
