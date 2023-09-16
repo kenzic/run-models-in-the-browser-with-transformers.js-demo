d8dd9fb8441adc5395eb0790b0b09caf0526a5ec
Chris McKenzie <christophermckenzie.84@gmail.com>
2023-09-16 15:38:11 -0400

    add summarization pipeline

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
 import { pipeline } from 'https://cdn.jsdelivr.net/npm/@xenova/transformers@2.3.0';
+
+const summarization = await pipeline(
+    'summarization', // task
+    'Xenova/t5-small' // model
+);
